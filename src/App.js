9c9a4b3f19be4b465f0b633c526f265f74734c1e
laureleya <sarah.bachinger@uni-jena.de>
2020-03-22 08:57:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleMapsComponent from "./GoogleMapsComponent";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <GoogleMapsComponent />
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
